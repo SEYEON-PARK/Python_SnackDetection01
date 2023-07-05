76e9889a01c56a1550dbb618044e8af0aa137754
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-07-06 01:41:31 +0900

    연습 코드

---
+++ practice/time_1.py
@@ -0,0 +1,8 @@
+import time
+from datetime import datetime
+now = datetime.now()
+
+print("현재 : ", now)
+print("현재 날짜 : ", now.date())
+print("현재 시간 : ", now.time())
+print("timestamp : ", now.timestamp())
