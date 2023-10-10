10b95005d9ecc1e5fd4a4a1c3283ce805583f97b
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-10-11 01:42:00 +0900

    안드로이드 카메라로도 인식할 수 있도록 하는 코드!

--- android.py
+++ android.py
@@ -98,7 +98,7 @@ while True: # 무한 반복
     cv2.imshow("Android_cam", frame) # frame 보여주기
     if(date!=""): # 만약, date가 빈 문자열이 아니라면
         tts = gTTS(text=date, lang='ko')
-        tts.save("snack.mp3")
+        tts.save("snack.mp3") # 음성 파일 저장하기
         playsound("snack.mp3")
         os.remove("snack.mp3") # 만든 파일은 바로 지우기!
         date="" # 해당 과자라는 메시지 말하는 거 한 번만 하게 하기 위해서 
