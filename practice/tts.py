81f8d385b19630e2cb5318a1abdaecd847b1f070
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-10-28 01:53:59 +0900

    pyttsx3 연습 코드!

--- practice/tts.py
+++ practice/tts.py
@@ -1,5 +1,5 @@
 import pyttsx3 # pyttsx3를 사용하기 위해
 s = pyttsx3.init() 
 data = "Sample Text" # 음성 파일로 만들 텍스트 문자열 data
-s.say(data) # 텍스트 문자열 말하게 하기!
+s.say(data) # 텍스트 문자열 말하게 하기!(음성 출력)
 s.runAndWait()  
