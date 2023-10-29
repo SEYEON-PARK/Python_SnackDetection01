b09ba836fb64e6c66b6473282af94224eac5fee9
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-10-30 02:19:58 +0900

    연습 파일

--- practice/main.py
+++ practice/main.py
@@ -31,7 +31,7 @@ while(True):
 	'''
 
 	# (xmin, ymin)은 왼쪽 상단 좌표고, (xmax, ymax)는 오른쪽 하단 좌표다!
-	print(len(results.pandas().xyxy[0].xmin))
+	print(len(results.pandas().xyxy[0].xmin)) # 탐지되는 객체의 수 출력하기
 	for i in range(len(results.pandas().xyxy[0].xmin)):
 		if(results.pandas().xyxy[0].name[i]=="Alsaeuchip" and results.pandas().xyxy[0].confidence[i]>0.7):
 			date="알새우칩입니다."
