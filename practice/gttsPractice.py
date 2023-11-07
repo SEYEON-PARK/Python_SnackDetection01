e33a859820805fc7fb802e3582564e5ab61ffd85
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-11-08 01:40:11 +0900

    gTTS 사용해보려고 시도해본 코드!(잘 실행되었음)

--- practice/gttsPractice.py
+++ practice/gttsPractice.py
@@ -1,7 +1,7 @@
 from gtts import gTTS # 구글에서 제공하는 TTS 서비스를 이용하기 위해
 from playsound import playsound # 음성 파일을 재생하기 위해
 
-text ="재밌게 놀아!"
+text ="재밌게 놀아!" # 텍스트 문자열
 
 tts = gTTS(text=text, lang='ko')
 tts.save("hello.mp3")
